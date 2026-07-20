aa4504c77b183527a5185f3138d170fce07e4a13
ayeshasiraj00 <sirajayesha242>
2026-07-20 21:59:32 +0500

    updated

--- chapter2/app.js
+++ chapter2/app.js
@@ -1,4 +1,17 @@
+// q1
 var username;
+// q2
 var myName = "Ayesha Siraj";
+// q3
 var message = "Hello World";
 alert ("message");
+// q4
+var studentName = "Ayesha Siraj";
+var age = "17 year old";
+var course = "Certified mobile Application Development";
+alert(studentName);
+alert(age);
+alert(course);
+// q5
+var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+alert(pizza);
